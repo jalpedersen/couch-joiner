045626168959ccc791396902816195054d4755c0
Jesper Andre Lyngesen Pedersen <jalpedersen@gmail.com>
2010-11-23 21:52:11 +0100

    Remove view if no map or reduce function is found

--- src/joiner/design.clj
+++ src/joiner/design.clj
@@ -10,12 +10,20 @@
 			    sum
 			    (assoc sum (keyword value) file-content))))]
     (reduce loader-fn {} ["map" "reduce"])))
-  
+
+(defn- remove-view [database design-doc view-name]
+  (with-db database
+    (let [doc (get-document (str "_design/" design-doc))
+	  views (:views doc)]
+      (update-document (assoc doc :views (dissoc views (keyword view-name)))))))
 
 (defn update-view [database design-doc view-name]
   "Create or update a new view based on the resources found at
    design-doc/view-name/[map.js reduce.js]"
-  (with-db database database
-    (save-view design-doc (keyword view-name)
-	       (load-view (str design-doc "/views/" view-name "/")))))
+  (let [mapreduce (load-view (str design-doc "/views/" view-name "/"))]
+    (if (empty? mapreduce)
+      (remove-view database design-doc view-name)
+      (with-db database database
+	(save-view design-doc (keyword view-name) mapreduce)))))
+	       
 
