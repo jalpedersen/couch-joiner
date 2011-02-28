56227723586272d4f756fa54bda63354811a16e1
Jesper Andre Lyngesen Pedersen <jalpedersen@gmail.com>
2011-02-28 17:25:06 +0300

    clutch 0.2.4

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "Manage couchdb users and databases"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-		 [com.ashafa/clutch "0.2.3-SNAPSHOT"]]
+		 [com.ashafa/clutch "0.2.4"]]
   :dev-dependencies [[swank-clojure "1.2.1"]
                      [lein-eclipse "1.0.0"]]
   :main joiner.main
