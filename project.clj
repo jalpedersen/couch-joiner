52c0b46ddac3353f895f9c56d1133d5bde39acc6
Jesper Andre Lyngesen Pedersen <jalpedersen@gmail.com>
2011-06-06 19:40:10 +0400

    version 0.8

--- project.clj
+++ project.clj
@@ -1,10 +1,11 @@
-(defproject joiner "1.0.0-SNAPSHOT"
+(defproject joiner "0.8"
   :description "Manage couchdb users and databases"
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]
 		 [com.ashafa/clutch "0.2.4"]]
   :dev-dependencies [[swank-clojure "1.2.1"]
-                     [lein-eclipse "1.0.0"]]
+                     [lein-eclipse "1.0.0"]
+                     [lein-clojars "0.6.0"]]
   :main joiner.main
   :warn-on-reflection true
   )
